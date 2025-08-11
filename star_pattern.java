d7487ffd4de15a226d0c3dc5dcd12a2d786ceb71
Aishi Srivastava <163891174+aishisrivastava@users.noreply.github.com>
2025-08-11 00:18:46 -0700

    Add files via upload

---
+++ star_pattern.java
@@ -0,0 +1,13 @@
+public class star_pattern {
+    public static void main(String[] args) {
+        int rows = 5;
+        int i, j;
+
+        for (i = 1; i <= rows; i++) {
+            for (j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
